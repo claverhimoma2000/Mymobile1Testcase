98183bb3cf0e1327480d12e2ae1e2ee27c756990
claver ibombo <claver ibombo@DESKTOP-3PB1645.attlocal.net>
2020-02-17 18:27:46 -0800

    First push

---
+++ com/emobilstore/mobileStore.java
@@ -0,0 +1,19 @@
+package com.emobilstore;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class mobileStore {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
